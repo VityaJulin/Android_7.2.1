0f5447857b0040f9487e01e4d9033784ba83d743
vityajulin <vkdvornokov@gmail.com>
2020-05-30 17:26:42 +0300

    Initial commit

---
+++ app/src/main/java/com/example/android_721/MainActivity.java
@@ -0,0 +1,21 @@
+package com.example.android_721;
+
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+    private EditText address;
+    private Button search;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        address = findViewById(R.id.edTxt_enter_address);
+        search = findViewById(R.id.btn_search);
+    }
+}
